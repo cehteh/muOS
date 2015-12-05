50f731ce0c9545b6d16db3754a00f99cce0d61f4
Christian Thaeter <ct@pipapo.org>
2015-12-05 22:37:10 +0100

    typo fixes and license header for makefiles

--- muos.mk
+++ muos.mk
@@ -1,3 +1,20 @@
+#      mµOS            - my micro OS
+#
+# Copyright (C)
+#      2015                            Christian Thäter <ct@pipapo.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 SOURCES += $(MAIN).c
 SOURCES += $(wildcard muos/*.c)
@@ -27,7 +44,7 @@ PROGRAMS += $(MAIN).elf
 
 PRINTFMT = printf "%-60s%16s\n"
 
-MAKEFLAGS = -R -s -O -j $(shell nproc || echo 2)
+MAKEFLAGS = -R -s -j $(shell nproc || echo 2)
 .DEFAULT_GOAL = all
 
 .SUFFIXES:
--- prg_avrdude.mk
+++ prg_avrdude.mk
@@ -1,3 +1,20 @@
+#      mµOS            - my micro OS
+#
+# Copyright (C)
+#      2015                            Christian Thäter <ct@pipapo.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 AVRDUDE = avrdude
 
@@ -19,7 +36,7 @@ eep: $(MAIN).eep
 	$(OBJCOPY) -j .eeprom  --change-section-lma .eeprom=0 -O ihex $< $@ 2>/dev/null
 
 program: all .v/AVRDUDE .v/AVRDUDE_FLAGS .v/AVRDUDE_DEFAULT_SCK .v/MAIN
-	printf $(PRINTFMT) $(MAIN) [PROGAM]
+	$(PRINTFMT) $(MAIN) [PROGRAM]
 	$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_DEFAULT_SCK) -U flash:w:$(MAIN).hex:i
 #-U eeprom:w:$(MAIN).eep:i
 
