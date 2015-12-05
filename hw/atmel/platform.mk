8d9ba06b428c7687f3b21c83236bd548102d243f
Christian Thaeter <ct@pipapo.org>
2015-12-05 21:31:49 +0100

    Add dependencies over Makefile variables

--- hw/atmel/platform.mk
+++ hw/atmel/platform.mk
@@ -14,15 +14,15 @@ CCFLAGS += -flto
 FUSE_DEFAULT = -U lfuse:w:0x62:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 #FUSE_RELEASE = -U lfuse:w:0x64:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 
-BUILD += $(MAIN).hex $(MAIN).eep
+PROGRAMS += $(MAIN).hex $(MAIN).eep
 
 elf: $(MAIN).elf
 
-%.elf: $(OBJECTS) muos/hw/$(PLATFORM)/platform.mk
+%.elf: $(OBJECTS) .v/LDFLAGS .v/CC
 	$(PRINTFMT) $@ [LINK]
 	$(CC) $(LDFLAGS) $(OBJECTS) --output $@ 2>&1 | sed 'h;:b;$b;N;N;/appears to be a misspelled signal handler/{N;d};$b;D'
 
-%.asm: %.elf
-	echo "$@ \t←\t $< \t\t\t[ASM]"
+%.asm: %.elf .v/OBJDUMP
+	$(PRINTFMT) $@ [ASM]
 	$(OBJDUMP) -S $< >$@
 
--- prg_avrdude.mk
+++ prg_avrdude.mk
@@ -3,37 +3,35 @@ AVRDUDE = avrdude
 
 AVRDUDE_PART = -p $(MCU)
 
-AVRDUDE_FLAGS = -c $(AVRDUDE_PROGRAMMER) $(AVRDUDE_PART) -b $(AVRDUDE_BAUDRATE) -P $(AVRDUDE_PORT)
-AVRDUDE_DEFAULT_SCK = -B 8
+AVRDUDE_FLAGS += -c $(AVRDUDE_PROGRAMMER) $(AVRDUDE_PART) -b $(AVRDUDE_BAUDRATE) -P $(AVRDUDE_PORT)
+AVRDUDE_DEFAULT_SCK += -B 8
 #AVRDUDE_128kHz_SCK = -B 16
-
-BUILDEXT = .elf
 
 hex: $(MAIN).hex
 eep: $(MAIN).eep
 
-%.hex: %.elf muos/prg_$(PROGRAMMER).mk
+%.hex: %.elf .v/OBJCOPY
 	$(PRINTFMT) $@ [HEX]
 	$(OBJCOPY) -O ihex -j .text $< $@
 
-%.eep: %.elf muos/prg_$(PROGRAMMER).mk
+%.eep: %.elf .v/OBJCOPY
 	$(PRINTFMT) $@ [EEP]
 	$(OBJCOPY) -j .eeprom  --change-section-lma .eeprom=0 -O ihex $< $@ 2>/dev/null
 
-program: all
+program: all .v/AVRDUDE .v/AVRDUDE_FLAGS .v/AVRDUDE_DEFAULT_SCK .v/MAIN
 	printf $(PRINTFMT) $(MAIN) [PROGAM]
 	$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_DEFAULT_SCK) -U flash:w:$(MAIN).hex:i
 #-U eeprom:w:$(MAIN).eep:i
 
-read_program:
+read_program: .v/AVRDUDE .v/AVRDUDE_FLAGS .v/AVRDUDE_DEFAULT_SCK .v/MAIN
 	$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_DEFAULT_SCK) -U flash:r:$(MAIN).hex:i -U eeprom:r:$(MAIN).eep:i
 
-reset_fuse:
-	$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_128kHz_SCK) $(FUSE_DEFAULT)
+#reset_fuse:
+#	$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_128kHz_SCK) $(FUSE_DEFAULT)
 
 #fuse_128khz:
 #	$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_DEFAULT_SCK) $(FUSE_RELEASE)
 
-reset_device:
-	$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_DEFAULT_SCK) -e $(FUSE_DEFAULT) || \
-	$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_128kHz_SCK) -e $(FUSE_DEFAULT)
+reset_device: .v/AVRDUDE .v/AVRDUDE_FLAGS .v/AVRDUDE_DEFAULT_SCK .v/FUSE_DEFAULT
+	$(AVRDUDE) $(AVRDUDE_FLAGS) $(AVRDUDE_DEFAULT_SCK) -e $(FUSE_DEFAULT)
+
