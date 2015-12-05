18caae34721791aaa10bff562973d63265510392
Christian Thaeter <ct@pipapo.org>
2015-12-05 15:08:48 +0100

    Simplify makefiles/depgen

--- muos.mk
+++ muos.mk
@@ -5,19 +5,11 @@ SOURCES += $(wildcard muos/hw/*.c)
 
 OBJECTS = $(SOURCES:.c=.o)
 
-MAKEFILES = Makefile
-MAKEFILES += muos/muos.mk
-MAKEFILES += muos/muos_rules.mk
-MAKEFILES += muos/prg_$(PROGRAMMER).mk
-MAKEFILES += muos/prg_$(PROGRAMMER)_rules.mk
-MAKEFILES += muos/hw/$(PLATFORM)/platform.mk
-MAKEFILES += muos/hw/$(PLATFORM)/platform_rules.mk
-
 # Common CC Flags
 CCFLAGS = -std=gnu99
 
 # Dependency generation Flags
-DEPFLAGS = -mmcu=$(MCU) -M -MP
+DEPFLAGS = -mmcu=$(MCU) -M -MP -MT $*.o
 
 # Preprocessor Flags
 CPPFLAGS = -I .
@@ -31,7 +23,53 @@ LDFLAGS += -Wl,--relax,--gc-sections
 LDFLAGS += -Xlinker --no-fatal-warnings
 
 # What files must be generated for 'make all'
-COMPILE=
+BUILD = $(MAIN).elf
+
+PRINTFMT = printf "%-60s%16s\n"
+
+MAKEFLAGS += -R -s -O -j $(shell nproc || echo 2)
+.DEFAULT_GOAL = all
+
+.SUFFIXES:
 
 include muos/prg_$(PROGRAMMER).mk
 include muos/hw/$(PLATFORM)/platform.mk
+-include $(SOURCES:.c=.d)
+
+
+all: $(BUILD)
+	$(PRINTFMT) '$(BUILD)' [BUILD]
+
+# Dependency generation and cleanup
+
+%.d: %.c
+	$(PRINTFMT) $@ [DEPGEN]
+	$(CC) $(DEPFLAGS) $(CPPFLAGS) $< | sed 's,^$*.o,$*.o $*.d,g' > $@
+
+%.o: %.d
+
+
+depclean:
+	rm -f $(SOURCES:.c=.d)
+
+%.o: %.c Makefile muos/muos.mk
+	$(PRINTFMT) $@ [COMPILE]
+	$(CC) $(CFLAGS) -c $< -o $@
+
+
+asm: $(MAIN).asm
+
+show_asm: $(MAIN).asm
+	less $(MAIN).asm
+
+
+size: $(BUILD)
+	@$(SIZE) --target=ihex $(BUILD)
+
+clean: depclean
+	rm -f *.elf *.a $(OBJECTS)
+
+mrproper: clean
+	rm -f $(BUILD)
+
+#gitclean: git stash, git clean -dfx
