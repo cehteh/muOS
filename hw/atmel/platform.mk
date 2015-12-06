5201b7188cf088b5a874b912cd8221321da740c4
Christian Thaeter <ct@pipapo.org>
2015-12-06 03:10:58 +0100

    Makefile cosmetics

--- hw/atmel/platform.mk
+++ hw/atmel/platform.mk
@@ -32,15 +32,15 @@ CCFLAGS += -flto=$(PARALLEL)
 FUSE_DEFAULT = -U lfuse:w:0x62:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 #FUSE_RELEASE = -U lfuse:w:0x64:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 
-PROGRAMS += $(MAIN).hex $(MAIN).eep
+IMAGES += $(MAIN).hex $(MAIN).eep
 
 elf: $(MAIN).elf
 
 %.elf: $(OBJECTS) .v/LDFLAGS .v/CC
-	$(PRINTFMT) $@ [LINK]
+	$(PRINTFMT) $@ LINK
 	$(CC) $(LDFLAGS) $(OBJECTS) --output $@ 2>&1 | sed 'h;:b;$b;N;N;/appears to be a misspelled signal handler/{N;d};$b;D'
 
 %.asm: %.elf .v/OBJDUMP
-	$(PRINTFMT) $@ [ASM]
+	$(PRINTFMT) $@ ASM
 	$(OBJDUMP) -S $< >$@
 
