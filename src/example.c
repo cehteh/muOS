ee00d9a7fceb92358ec6cb000341d2b1d5f54abe
Christian Thaeter <ct@pipapo.org>
2015-12-18 20:37:48 +0100

    example cleanup

--- src/example.c
+++ src/example.c
@@ -2,6 +2,7 @@
  * add your license here
  */
 
+#include <stdlib.h>
 #include <util/delay_basic.h>
 
 
@@ -9,44 +10,45 @@
 #include <muos/rtq.h>
 #include <muos/bgq.h>
 #include <muos/clpq.h>
+#include <muos/clock.h>
 
 // define events, no main here
 
-void
-blink_led_time (const struct muos_spriq_entry* event)
+void blink_led (void)
 {
-  PINB = _BV(PINB5);
-  (void) event;
-  //muos_repeat (event, 1000);
-}
-
-
-
-void blink_led(void)
-{
-  PINB = _BV(PINB5);
+  PINB = _BV(PINB5) |  _BV(PINB4);
   muos_bgq_pushback (blink_led);
 }
 
 
-
-void wait_a_bit(intptr_t amount)
+void toggle_red_timed (const struct muos_spriq_entry* event)
 {
-  for (intptr_t i = 1000; i; --i)
-    _delay_loop_2(amount);
+  PIND = _BV(PIND2);
+  muos_clpq_at (event->when, 250, toggle_red_timed);
+}
 
-  muos_bgq_pushback_arg (wait_a_bit, amount+1);
+void toggle_yellow_timed (const struct muos_spriq_entry* event)
+{
+  PINB = _BV(PINB5);
+  muos_clpq_at (event->when, 240, toggle_yellow_timed);
+}
+
+void toggle_green_timed (const struct muos_spriq_entry* event)
+{
+  PIND = _BV(PIND3);
+  muos_clpq_at (event->when, 232, toggle_green_timed);
 }
 
 
 void
 init (void)
 {
-  DDRB = _BV(PINB5);
+  DDRB = _BV(PINB5) | _BV(PINB4);
+  DDRD = _BV(PIND2) | _BV(PIND3);
 
-  muos_bgq_pushback (blink_led);
-  muos_bgq_pushback_arg (wait_a_bit, 10);
-  //muos_at (1000, blink_led_time);
+  muos_clpq_at (0, 0, toggle_green_timed);
+  muos_clpq_at (0, 0, toggle_red_timed);
+  muos_clpq_at (0, 0, toggle_yellow_timed);
 }
 
 
