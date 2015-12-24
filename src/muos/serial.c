e71c4f335d66e335034c686d15955d6e11b0a230
Christian Thaeter <ct@pipapo.org>
2015-12-24 07:47:43 +0100

    serial lib, first go

---
+++ src/muos/serial.c
@@ -0,0 +1,121 @@
+/*
+ *      mµOS            - my micro OS
+ *
+ * Copyright (C)
+ *      2015                            Christian Thäter <ct@pipapo.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <muos/muos.h>
+#include <muos/serial.h>
+#include <muos/rtq.h>
+
+//TODO: errorhandling
+
+#if MUOS_SERIAL_TXBUFFER > 1
+muos_txbuffer_type muos_txbuffer;
+#endif
+
+#if MUOS_SERIAL_RXBUFFER > 1
+muos_rxbuffer_type muos_rxbuffer;
+#endif
+
+void
+muos_serial_init (void)
+{
+  muos_hw_serial_init ();
+
+#if MUOS_SERIAL_TXBUFFER > 0
+  muos_hw_tx_enable ();
+#endif
+
+#if MUOS_SERIAL_RXBUFFER > 0
+  muos_hw_rx_enable ();
+#endif
+}
+
+
+extern void
+MUOS_SERIAL_RXCALLBACK (void);
+
+
+MUOS_HW_ISR(MUOS_HW_SERIAL_ISRNAME_TX_READY(MUOS_SERIAL_HW))
+{
+  if (muos_txbuffer.descriptor.len)
+    {
+      MUOS_SERIAL_TX_REGISTER = MUOS_CBUFFER_POP (muos_txbuffer);
+      if (!muos_txbuffer.descriptor.len)
+        muos_hw_serial_tx_stop ();
+    }
+}
+
+
+MUOS_HW_ISR(MUOS_HW_SERIAL_ISRNAME_RX_AVAILABLE(MUOS_SERIAL_HW))
+{
+  if (MUOS_BUFFER_FREE(muos_rxbuffer))
+    {
+      //TODO: errorhandling
+      MUOS_BUFFER_PUSH (muos_rxbuffer, MUOS_SERIAL_RX_REGISTER);
+      if (!muos_status.serial_rxrtq_pending)
+        {
+          muos_status.serial_rxrtq_pending = true;
+          muos_rtq_pushback (MUOS_SERIAL_RXCALLBACK);
+        }
+    }
+}
+
+static
+void wait_for_tx (muos_cbuffer_index requested)
+{
+  while (MUOS_CBUFFER_FREE (muos_txbuffer) < requested)
+    {
+      //DEBUG: txbuffer_full
+      muos_hw_serial_tx_run ();
+      muos_sleep ();
+      muos_hw_serial_tx_stop ();
+    }
+}
+
+
+void
+muos_serial_tx_byte (uint8_t b)
+{
+  muos_hw_serial_tx_stop ();
+  wait_for_tx (1);
+  MUOS_CBUFFER_PUSH (muos_txbuffer, b);
+  muos_hw_serial_tx_run ();
+}
+
+
+
+bool
+muos_serial_rx_pending (void)
+{
+  volatile bool ret;
+  muos_hw_serial_rx_stop ();
+  if (MUOS_BUFFER_USED(muos_rxbuffer))
+    {
+      ret = true;
+    }
+  else
+    {
+      ret = false;
+      muos_status.serial_rxrtq_pending = false;
+    }
+  muos_hw_serial_rx_run ();
+  return ret;
+}
+
+
