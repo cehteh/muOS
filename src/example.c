3165ab2cb270fd8f8a98684774a0addd2459e6c4
Christian Thaeter <ct@pipapo.org>
2015-12-24 09:13:12 +0100

    example update, working

--- src/example.c
+++ src/example.c
@@ -4,6 +4,7 @@
 
 #include <stdlib.h>
 #include <util/delay_basic.h>
+#include <avr/pgmspace.h>
 
 
 #include <muos/muos.h>
@@ -12,15 +13,18 @@
 #include <muos/bgq.h>
 #include <muos/clpq.h>
 #include <muos/clock.h>
+#include <muos/serial.h>
+#include <muos/io.h>
 
 // define events, no main here
-
 
 void
 toggle_red_timed (const struct muos_spriq_entry* event)
 {
   PIND = _BV(PIND2);
-  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (500));
+  muos_output_cstr ("foobar");
+  muos_clpq_repeat (event, MUOS_CLOCK_SECONDS (1));
+  (void) event;
 }
 
 void
@@ -37,6 +41,17 @@ toggle_green_timed (const struct muos_spriq_entry* event)
   muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (250));
 }
 
+void
+serial_echo (void)
+{
+  while (muos_serial_rx_pending ())
+    {
+      muos_serial_tx_byte (muos_rxbuffer.buffer[0]);
+      muos_hw_serial_rx_stop ();
+      MUOS_BUFFER_POP (muos_rxbuffer, 1);
+      muos_hw_serial_rx_run ();
+    }
+}
 
 void
 init (void)
@@ -44,9 +59,11 @@ init (void)
   DDRB = _BV(PINB5) | _BV(PINB4);
   DDRD = _BV(PIND2) | _BV(PIND3);
 
-  muos_clpq_at (0, 0, toggle_green_timed);
+  muos_serial_init ();
+
+  //muos_clpq_at (0, 0, toggle_green_timed);
   muos_clpq_at (0, 0, toggle_red_timed);
-  muos_clpq_at (0, 0, toggle_yellow_timed);
+  //muos_clpq_at (0, 0, toggle_yellow_timed);
 }
 
 
