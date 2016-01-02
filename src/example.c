c48be4b25dc5f36568ad6b6391af92b5072a440b
Christian Thaeter <ct@pipapo.org>
2016-01-02 20:34:15 +0100

    call errorhandling function with interrupts disabled
    take care that the clearerr will not be called when one is already pending

--- src/example.c
+++ src/example.c
@@ -22,7 +22,9 @@
 void
 serial_ping (const struct muos_spriq_entry* event)
 {
-  muos_output_char ('.');
+  if (!muos_status.lineedit_pending)
+    muos_output_char ('.');
+
   muos_clpq_repeat (event, MUOS_CLOCK_SECONDS (1));
 }
 
@@ -30,21 +32,21 @@ void
 toggle_red_timed (const struct muos_spriq_entry* event)
 {
   PIND = _BV(PIND2);
-  muos_clpq_repeat (event, MUOS_CLOCK_SECONDS (1));
+  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (25));
 }
 
 void
 toggle_yellow_timed (const struct muos_spriq_entry* event)
 {
   PINB = _BV(PINB5);
-  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (125));
+  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (250));
 }
 
 void
 toggle_green_timed (const struct muos_spriq_entry* event)
 {
   PIND = _BV(PIND3);
-  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (50));
+  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (20));
 }
 
 
@@ -55,44 +57,30 @@ clearerror (const struct muos_spriq_entry* event)
   PORTD &= ~_BV(PIND2);
 }
 
+
 void
 error (void)
 {
-  if (muos_error_pending ())
+  if (!(PORTD & _BV(PIND2)))
     {
       PORTD |= _BV(PIND2);
+      muos_clpq_at_unsafe (muos_now (), MUOS_CLOCK_SECONDS (1), clearerror);
+    }
 
 #define MUOS_ERROR(name)                                         \
-      if (muos_error_check (muos_##name))                        \
-        {                                                        \
-        }
+  if (muos_error_check (muos_##name))                            \
+    {                                                            \
+    }
 
   MUOS_ERRORS;
 #undef MUOS_ERROR
-    }
-
-  muos_clpq_at (muos_now (), MUOS_CLOCK_SECONDS (1), clearerror);
 }
 
 
-void
-serial_blinkerr (const struct muos_spriq_entry* event)
-{
-  if (muos_error_pending ())
-    {
-#define MUOS_ERROR(name)                                         \
-      if (muos_error_check (muos_##name))                        \
-        {                                                        \
-          PIND = _BV(PIND2);                                     \
-        }
 
-  MUOS_ERRORS;
-#undef MUOS_ERROR
-    }
 
-  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (50));
-}
 
+#if 0
 void
 serial_printerr (const struct muos_spriq_entry* event)
 {
@@ -112,6 +100,7 @@ serial_printerr (const struct muos_spriq_entry* event)
 
   muos_clpq_repeat (event, MUOS_CLOCK_SECONDS (2));
 }
+#endif
 
 void
 serial_echo (void)
@@ -137,9 +126,9 @@ init (void)
   muos_serial_init ();
 
   //muos_clpq_at (0, 0, toggle_red_timed);
-  //muos_clpq_at (0, 0, toggle_yellow_timed);
+  muos_clpq_at (0, 0, toggle_yellow_timed);
   muos_clpq_at (0, 0, toggle_green_timed);
-  //  muos_clpq_at (0, 0, serial_ping);
+  muos_clpq_at (0, 0, serial_ping);
   //muos_clpq_at (0, 0, serial_blinkerr);
 }
 
