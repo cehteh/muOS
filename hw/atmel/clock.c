ffa272204c7fc9f0c918bfa08269dae51de8386a
Christian Thaeter <ct@pipapo.org>
2016-03-19 16:21:38 +0100

    add drift compensation for clock calibration

--- hw/atmel/clock.c
+++ hw/atmel/clock.c
@@ -36,10 +36,14 @@ ISR(ISRNAME_OVERFLOW(MUOS_CLOCK_HW))
 EMPTY_INTERRUPT(ISRNAME_COMPMATCH(MUOS_CLOCK_HW, MUOS_CLOCK_HW_COMPAREMATCH));
 
 
-#if MUOS_CLOCK_CALIBRATE == 1
-static muos_clock muos_clock_calibrate_last;
+#if MUOS_CLOCK_CALIBRATE != 0
+static muos_clock calibrate_last;
 
-//PLANNED: add long time calibration to keep frequency more stable
+#ifdef MUOS_CLOCK_CALIBRATE_DRIFT
+static muos_clock calibrate_drift_last;
+static muos_clock calibrate_drift_sync;
+#endif
+
 
 void
 muos_clock_calibrate (muos_clock sync)
@@ -50,12 +54,14 @@ muos_clock_calibrate (muos_clock sync)
     {
       muos_clock elapsed;
 
-      if (now > muos_clock_calibrate_last)
-        elapsed = now - muos_clock_calibrate_last;
+      if (now > calibrate_last)
+        elapsed = now - calibrate_last;
       else
-        elapsed = muos_clock_calibrate_last - now;
+        elapsed = calibrate_last - now;
 
-      if (elapsed > sync)
+
+#ifndef MUOS_CLOCK_CALIBRATE_DRIFT
+        if (elapsed > sync)
         {
           if (OSCCAL != 128)
             --OSCCAL;
@@ -69,8 +75,47 @@ muos_clock_calibrate (muos_clock sync)
           else
             OSCCAL = MUOS_HW_ATMEL_OSCAL_HIGHSWITCH;
         }
+#else
+      if (calibrate_drift_sync + sync > MUOS_CLOCK_CALIBRATE_DRIFT)
+        calibrate_drift_last += sync;
+      else
+        calibrate_drift_sync += sync;
+
+      muos_clock elapsed_drift;
+
+      if (now > calibrate_drift_last)
+        elapsed_drift = now - calibrate_drift_last;
+      else
+        elapsed_drift = calibrate_drift_last - now;
+
+      int8_t divert = (elapsed>sync?1:elapsed<sync?-1:0) +
+        (elapsed_drift>calibrate_drift_sync?1:elapsed_drift<calibrate_drift_sync?-1:0);
+
+      if (divert > 0)
+        {
+          if (OSCCAL != 128)
+            --OSCCAL;
+          else
+            OSCCAL = MUOS_HW_ATMEL_OSCAL_LOWSWITCH;
+        }
+      else if (divert < 0)
+        {
+          if (OSCCAL != 127)
+            ++OSCCAL;
+          else
+            OSCCAL = MUOS_HW_ATMEL_OSCAL_HIGHSWITCH;
+        }
+#endif
     }
 
-  muos_clock_calibrate_last = now;
+#ifdef MUOS_CLOCK_CALIBRATE_DRIFT
+  else
+    {
+      calibrate_drift_sync = 0;
+      calibrate_drift_last = now;
+    }
+#endif
+
+  calibrate_last = now;
 }
 #endif
