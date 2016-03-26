fa9bd5791e6cfd4176623b77cbda5bee9b5b234d
Christian Thaeter <ct@pipapo.org>
2016-03-26 23:45:25 +0100

    update example/config for attiny84 w/ cppm parser

--- src/example.c
+++ src/example.c
@@ -16,6 +16,7 @@
 #include <muos/serial.h>
 #include <muos/io.h>
 #include <muos/error.h>
+#include <muos/cppm.h>
 
 // define events, no main here
 
@@ -23,11 +24,28 @@
 void
 toggle_led_timed (const struct muos_spriq_entry* event)
 {
-  PINB = _BV(PINB1);
-  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (250));
+  PINA = _BV(PINA6);
+  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (100));
 }
 
 
+
+void
+toggle_led_timed2 (const struct muos_spriq_entry* event)
+{
+  PINA = _BV(PINA0) | _BV(PINA1) | _BV(PINA2) | _BV(PINA3) | _BV(PINA4) | _BV(PINA5) | _BV(PINA6);
+  muos_clpq_repeat (event, MUOS_CLOCK_MICROSECONDS (2000));
+}
+
+
+void
+toggle_led (void)
+{
+  PINA = _BV(PINA5);
+  PINA = _BV(PINA5);
+  muos_bgq_pushback (toggle_led);
+  PINA = _BV(PINA5);
+}
 
 void
 error (void)
@@ -40,19 +58,62 @@ error (void)
 }
 
 
+
+void
+cppm_output (void)
+{
+  PINA = _BV(PINA2);
+
+  if (muos_cppm_channel[0] > MUOS_CLOCK_MICROSECONDS(1500))
+    PORTA |= _BV(PINA0);
+  else
+    PORTA &= ~_BV(PINA0);
+
+  if (muos_cppm_channel[1] > MUOS_CLOCK_MICROSECONDS(1500))
+    PORTA |= _BV(PINA1);
+  else
+    PORTA &= ~_BV(PINA1);
+
+#if 0
+  if (muos_cppm_channel[2] > MUOS_CLOCK_MICROSECONDS(1500))
+    PORTA |= _BV(PINA2);
+  else
+    PORTA &= ~_BV(PINA2);
+
+  if (muos_cppm_channel[3] > MUOS_CLOCK_MICROSECONDS(1500))
+    PORTA |= _BV(PINA3);
+  else
+    PORTA &= ~_BV(PINA3);
+
+  if (muos_cppm_channel[4] > MUOS_CLOCK_MICROSECONDS(1500))
+    PORTA |= _BV(PINA4);
+  else
+    PORTA &= ~_BV(PINA4);
+  if (muos_cppm_channel[5] > MUOS_CLOCK_MICROSECONDS(1500))
+    PORTA |= _BV(PINA5);
+  else
+    PORTA &= ~_BV(PINA5);
+
+  if (muos_cppm_channel[6] > MUOS_CLOCK_MICROSECONDS(1500))
+    PORTA |= _BV(PINA6);
+  else
+    PORTA &= ~_BV(PINA6);
+#endif
+}
+
+
+
 void
 init (void)
 {
-  DDRB = _BV(PINB1);
-  PORTB |= _BV(PINB1);
-  GIMSK |= _BV(PCIE);
-  PCMSK |= _BV(PCINT2);
+  DDRA = _BV(PINA0) | _BV(PINA1) | _BV(PINA2) | _BV(PINA3) | _BV(PINA4) | _BV(PINA5) | _BV(PINA6);
+  //  PINA = _BV(PINA0) | _BV(PINA2) | _BV(PINA4) | _BV(PINA6);
 
-  muos_clpq_at (0, MUOS_CLOCK_MILLISECONDS (250), toggle_led_timed);
-}
+  // PINA = _BV(PINA1);
+  //  GIMSK |= _BV(PCIE);
+  //PCMSK |= _BV(PCINT2);
 
-ISR(PCINT0_vect)
-{
-  if (PINB & _BV(PINB2))
-    muos_clock_calibrate (muos_clock_now (), MUOS_CLOCK_MILLISECONDS (20));
+  muos_clpq_at (0, MUOS_CLOCK_MILLISECONDS (100), toggle_led_timed);
+  //muos_clpq_at (0, MUOS_CLOCK_MILLISECONDS (1), toggle_led_timed2);
+  muos_bgq_pushback (toggle_led);
 }
