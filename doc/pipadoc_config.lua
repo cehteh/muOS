41d7be932982d2bf798615c3d9fe79b880704b9f
Christian Thaeter <ct@pipapo.org>
2016-04-15 10:06:07 +0200

    DOC: add git-blame support to pipadoc

--- doc/pipadoc_config.lua
+++ doc/pipadoc_config.lua
@@ -6,16 +6,48 @@ preprocessor_register ("^lua$",
                        function (str) return str:gsub("{FUNCTION}", '+{CONTEXT.PRE:match("function ([^)]*%%))")}+::{DOCVARS.NL} ') end
 )
 
+
+function git_blame (file, line)
+  local result = {}
+  local git = io.popen("git blame '"..file.."' -L "..tostring(line)..",+1 -p 2>/dev/null")
+
+  for line in git:lines() do
+    local k,v = line:match("^([%a-]+) (.*)")
+    if line:match("^([%w]+) (%d+) ") then
+      k = 'revision'
+      v = line:match("^(%w*)")
+    elseif line:match("^\t") then
+      k = 'line'
+      v = line:match("^\t(.*)")
+    end
+    result[k] = v
+  end
+  local _,_,exitcode = git:close()
+  return exitcode == 0 and result or nil
+end
+
+function git_blame_context ()
+  local blame = git_blame (CONTEXT.FILE, CONTEXT.LINE)
+  if blame then
+    return " +"..DOCVARS.NL..
+      "  _"..blame.summary.."_ +"..DOCVARS.NL..
+      "  "..blame.author.." "..os.date("%c", tonumber(blame["author-time"])).." +"..DOCVARS.NL..
+      "  +"..tostring(blame.revision).."+"
+  else
+    return ""
+  end
+end
+
 preprocessor_register ("",
-                       function (str) return str:gsub("FIXME:([^ ]*) (.*)", 'FIXME:%1 {CONTEXT.FILE}:{CONTEXT.LINE}::{DOCVARS.NL}  %2') end
+                       function (str) return str:gsub("FIXME:([^ ]*) (.*)", 'FIXME:%1 {CONTEXT.FILE}:{CONTEXT.LINE}::{DOCVARS.NL}  %2{git_blame_context ()}{DOCVARS.NL}') end
 )
 
 preprocessor_register ("",
-                       function (str) return str:gsub("TODO:([^ ]*) (.*)", 'TODO:%1 {CONTEXT.FILE}:{CONTEXT.LINE}::{DOCVARS.NL}  %2') end
+                       function (str) return str:gsub("TODO:([^ ]*) (.*)", 'TODO:%1 {CONTEXT.FILE}:{CONTEXT.LINE}::{DOCVARS.NL}  %2{git_blame_context ()}{DOCVARS.NL}') end
 )
 
 preprocessor_register ("",
-                       function (str) return str:gsub("PLANNED:([^ ]*) (.*)", 'PLANNED:%1 {CONTEXT.FILE}:{CONTEXT.LINE}::{DOCVARS.NL}  %2') end
+                       function (str) return str:gsub("PLANNED:([^ ]*) (.*)", 'PLANNED:%1 {CONTEXT.FILE}:{CONTEXT.LINE}::{DOCVARS.NL}  %2{git_blame_context ()}{DOCVARS.NL}') end
 )
 
 --PLANNED: noweb like preprocessing syntax for chapter substitutions in textfiles
