8406d2b13e7a8e72ad6296abdc85d9c64a2ec271
Christian Thaeter <ct@pipapo.org>
2016-04-20 16:05:13 +0200

    FIX: clock prescaler definitions for mega328p

--- src/muos/hw/atmel/avr/atmega328p.h
+++ src/muos/hw/atmel/avr/atmega328p.h
@@ -36,13 +36,12 @@
 #define MUOS_HW_CLOCK0_DIV256   _BV(CS02)
 #define MUOS_HW_CLOCK0_DIV1024  _BV(CS02) | _BV(CS00)
 
-//FIXME: m328p settings
-//#define MUOS_HW_CLOCK1_OFF      0
-//#define MUOS_HW_CLOCK1_DIV1     _BV(CS10)
-//#define MUOS_HW_CLOCK1_DIV8     _BV(CS11)
-//#define MUOS_HW_CLOCK1_DIV64    _BV(CS11) | _BV(CS10)
-//#define MUOS_HW_CLOCK1_DIV256   _BV(CS12)
-//#define MUOS_HW_CLOCK1_DIV1024  _BV(CS12) | _BV(CS10)
+#define MUOS_HW_CLOCK1_OFF      0
+#define MUOS_HW_CLOCK1_DIV1     _BV(CS10)
+#define MUOS_HW_CLOCK1_DIV8     _BV(CS11)
+#define MUOS_HW_CLOCK1_DIV64    _BV(CS11) | _BV(CS10)
+#define MUOS_HW_CLOCK1_DIV256   _BV(CS12)
+#define MUOS_HW_CLOCK1_DIV1024  _BV(CS12) | _BV(CS10)
 
 static inline void
 muos_hw_shutdown (void)
