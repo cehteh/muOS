7d085972d4492359b247a59f9dd24dc554b1c046
Christian Thaeter <ct@pipapo.org>
2016-04-29 17:04:34 +0200

    FIX: io: return muos_error

--- io.h
+++ io.h
@@ -26,6 +26,7 @@
 
 #include <muos/error.h>
 
+//PLANNED: #define MUOS_IO .. if undefined then avr-libc stdio, check size requirement if it makes sense
 
 struct fmtconfig_type {
   uint8_t base:6;
@@ -122,7 +123,7 @@ ctrl
 
 
 
-#if MUOS_SERIAL_TXQUEUE == 0
+#ifndef MUOS_SERIAL_TXQUEUE
 
 #define OUTPUTFN(name, ...) muos_error muos_output_##name (__VA_ARGS__);
 MUOS_OUTPUTFNS
@@ -134,128 +135,128 @@ MUOS_OUTPUTFNS
 MUOS_OUTPUTFNS
 #undef OUTPUTFN
 
-static inline void
+static inline muos_error
 muos_output_char (char c)
 {
-  muos_txqueue_output_char (c);
+  return muos_txqueue_output_char (c);
 }
 
 
-static inline void
+static inline muos_error
 muos_output_cstr (const char* str)
 {
-  muos_txqueue_output_cstr (str);
+  return muos_txqueue_output_cstr (str);
 }
 
 
-static inline void
+static inline muos_error
 muos_output_cstrn (const char* str, uint8_t n)
 {
-  muos_txqueue_output_cstrn (str, n);
+  return muos_txqueue_output_cstrn (str, n);
 }
 
 
-static inline void
+static inline muos_error
 muos_output_fstr (muos_flash_cstr str)
 {
-  muos_txqueue_output_fstr (str);
+  return muos_txqueue_output_fstr (str);
 }
 
 
 
-static inline void
+static inline muos_error
 muos_output_mem (const uint8_t* mem, uint8_t len)
 {
   (void) mem;
   (void) len;
-
+  return muos_error_error;
 }
 
 
-static inline void
+static inline muos_error
 muos_output_nl (void)
 {
-   muos_txqueue_output_nl ();
+   return muos_txqueue_output_nl ();
 }
 
 
-static inline void
+static inline muos_error
 muos_output_csi_char (const char c)
 {
-  muos_txqueue_output_csi_char (c);
+  return muos_txqueue_output_csi_char (c);
 }
 
 
-static inline void
+static inline muos_error
 muos_output_csi_cstr (const char* str)
 {
-  muos_txqueue_output_csi_cstr (str);
+  return muos_txqueue_output_csi_cstr (str);
 }
 
 
-static inline void
+static inline muos_error
 muos_output_csi_fstr (muos_flash_cstr str)
 {
-  muos_txqueue_output_csi_fstr (str);
+  return muos_txqueue_output_csi_fstr (str);
 }
 
 
-static inline void
+static inline muos_error
 muos_output_intptr (intptr_t n)
 {
-  muos_txqueue_output_intptr (n);
+  return muos_txqueue_output_intptr (n);
 }
 
-static inline void
+static inline muos_error
 muos_output_uintptr (uintptr_t n)
 {
-  muos_txqueue_output_uintptr (n);
+  return muos_txqueue_output_uintptr (n);
 }
 
-static inline void
+static inline muos_error
 muos_output_int8 (int8_t n)
 {
-  muos_txqueue_output_int8 (n);
+  return muos_txqueue_output_int8 (n);
 }
 
-static inline void
+static inline muos_error
 muos_output_uint8 (uint8_t n)
 {
-  muos_txqueue_output_uint8 (n);
+  return muos_txqueue_output_uint8 (n);
 }
 
-static inline void
+static inline muos_error
 muos_output_int16 (uint16_t n)
 {
-  muos_txqueue_output_int16 (n);
+  return muos_txqueue_output_int16 (n);
 }
 
-static inline void
+static inline muos_error
 muos_output_uint16 (uint16_t n)
 {
-  muos_txqueue_output_uint16 (n);
+  return muos_txqueue_output_uint16 (n);
 }
 
-static inline void
+static inline muos_error
 muos_output_int32 (int32_t n)
 {
-  muos_txqueue_output_int32 (n);
+  return muos_txqueue_output_int32 (n);
 }
 
-static inline void
+static inline muos_error
 muos_output_uint32 (uint32_t n)
 {
-  muos_txqueue_output_uint32 (n);
+  return muos_txqueue_output_uint32 (n);
 }
 
 #if 0 //PLANNED: 64 bit
-static inline void
+static inline muos_error
 muos_output_int64 (int64_t n)
 {
   (void) n;
 }
 
-static inline void
+static inline muos_error
 muos_output_uint64 (uint64_t n)
 {
   (void) n;
@@ -263,65 +264,65 @@ muos_output_uint64 (uint64_t n)
 #endif
 
 #if 0
-static inline void
+static inline muos_error
 muos_output_float (float)
 {
   (void) str;
 }
 
 
-static inline void
+static inline muos_error
 muos_output_cstr_R ()
 {
   (void) str;
 }
 
-static inline void
+static inline muos_error
 muos_output_mem_R ()
 {
   (void) str;
 }
 
-static inline void
+static inline muos_error
 muos_output_int16_R ()
 {
   (void) str;
 }
 
-static inline void
+static inline muos_error
 muos_output_uint16_R ()
 {
   (void) str;
 }
 
-static inline void
+static inline muos_error
 muos_output_int32_R ()
 {
   (void) str;
 }
 
-static inline void
+static inline muos_error
 muos_output_uint32_R ()
 {
   (void) str;
 
 }
 
-static inline void
+static inline muos_error
 muos_output_int64_R ()
 {
   (void) str;
 
 }
 
-static inline void
+static inline muos_error
 muos_output_uint64_R ()
 {
   (void) str;
 
 }
 
-static inline void
+static inline muos_error
 muos_output_float_R ()
 {
   (void) str;
@@ -329,31 +330,31 @@ muos_output_float_R ()
 }
 #endif
 
-static inline void
+static inline muos_error
 muos_output_upcase (bool upcase)
 {
-  muos_txqueue_output_upcase (upcase);
+  return muos_txqueue_output_upcase (upcase);
 }
 
 
-static inline void
+static inline muos_error
 muos_output_base (uint8_t base)
 {
-  muos_txqueue_output_base (base);
+  return muos_txqueue_output_base (base);
 }
 
 
-static inline void
+static inline muos_error
 muos_output_pupcase (bool upcase)
 {
-  muos_txqueue_output_pupcase (upcase);
+  return muos_txqueue_output_pupcase (upcase);
 }
 
 
-static inline void
+static inline muos_error
 muos_output_pbase (uint8_t base)
 {
-  muos_txqueue_output_pbase (base);
+  return muos_txqueue_output_pbase (base);
 }
 
 
@@ -365,21 +366,21 @@ muos_output_pbase (uint8_t base)
 
 #if 0
 
-static inline void
+static inline muos_error
 muos_output_ifmt (uint8_t, uint8_t)
 {
   (void) str;
 
 }
 
-static inline void
+static inline muos_error
 muos_output_ffmt (char)
 {
   (void) str;
 
 }
 
-static inline void
+static inline muos_error
 muos_output_pcase (char)
 {
   (void) str;
@@ -394,14 +395,14 @@ muos_output_pbase (uint8_t)
 }
 
 
-static inline void
+static inline muos_error
 muos_output_pifmt (uint8_t, uint8_t)
 {
   (void) str;
 
 }
 
-static inline void
+static inline muos_error
 muos_output_pffmt (char)
 {
 
@@ -409,7 +410,7 @@ muos_output_pffmt (char)
 }
 
 
-static inline void
+static inline muos_error
 muos_output_ctrl (uint8_t, uint8_t, uint8_t)
 {
 
