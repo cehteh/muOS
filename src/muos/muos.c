c0f02c64839051db0bfa7779bc50116331ccbd1e
Christian Thaeter <ct@pipapo.org>
2016-04-30 13:22:30 +0200

    doc: move yield/wait doc to .h add missing declarations

--- src/muos/muos.c
+++ src/muos/muos.c
@@ -67,35 +67,6 @@ muos_start (void)
 static uint8_t sched_depth_;
 
 
-//muos_api:
-//: .Wait for some condition come true
-//: ----
-//: muos_error muos_wait (muos_wait_fn fn, intptr_t param, muos_shortclock timeout)
-//: ----
-//: +fn+::
-//:   function checking for some condition, must return 'false' while the condition
-//:   is not met and finally 'true' on success. Can be NULL, then 'muos_wait()' uses
-//:   only the timeout for the wait.
-//: +param+::
-//:   an optinal intptr_t argument passed to the test function
-//: +timeout+::
-//:   time limit for the wait
-//:
-//: Calls a recursive mainloop and with testing for a given condition for some time.
-//: Returns 'true' when the condition succeeded and 'false' on timeout or recursion limit.
-//:
-//: Care must be taken that I/O (and other things) are not anymore in order when the
-//: mainloop is called recursively. Often it is better to avoid waiting and divide the
-//: work into smaller tasks which are put in order on the work queues.
-//:
-//: Because of stack limits entering the mainloop recursively is limited. One should always
-//: expect that a wait can return instantly with 'false' and the error 'muos_warn_sched_depth'
-//: being flaged.
-//:
-//: .Returns
-//:   'muos_success':: the wait condition got met
-//:   'muos_warn_sched_depth':: depth limit for recursive mainloops hit
-//:   'muos_warn_wait_timeout':: timed out
 muos_error
 muos_wait (muos_wait_fn fn, intptr_t param, muos_shortclock timeout)
 {
@@ -151,26 +122,6 @@ muos_wait (muos_wait_fn fn, intptr_t param, muos_shortclock timeout)
 }
 
 
-//muos_api:
-//: .Enter Mainloop recursively, scheduling other jobs
-//: ----
-//: muos_error muos_yield (uint8_t count)
-//: ----
-//: +count+::
-//:   number of jobs to schedule, must be less than 254
-//:
-//: Calls a recursive mainloop executing at most 'count' jobs. A job here is any one
-//: thing queued on any of the works queues. Returns if nothing left to do the
-//: 'count' limit got reached or the scheduler depth limit got reached. Same precautions
-//: as on 'muos_wait()' apply.
-//:
-//: Yielding id applicable when one has code which loops for some extended time but shall
-//: not stall the work to be done *and* this code will never be called recursively.
-//:
-//: .Returns
-//:   'muos_warn_sched_depth':: depth limit for recursive mainloops hit
-//:   'muos_success':: yielded at most 'count' times
-//PLANNED: add priority, which queues to schedule
 muos_error
 muos_yield (uint8_t count)
 {
