1d37ee65552236f83f583940398422d636fd850f
Christian Thäter <ct@wolke.pipapo.org>
2018-11-15 16:57:31 +0100

    Simplify the 'example.c' only LED blink and serial echo
    serial at 9600 baud

--- src/example.c
+++ src/example.c
@@ -3,7 +3,6 @@
  */
 
 #include <stdlib.h>
-#include <util/delay_basic.h>
 #include <avr/pgmspace.h>
 
 
@@ -16,7 +15,6 @@
 #include <muos/serial.h>
 #include <muos/io.h>
 #include <muos/error.h>
-#include <muos/cppm.h>
 
 // define events, no main here
 
@@ -24,28 +22,11 @@
 void
 toggle_led_timed (const struct muos_spriq_entry* event)
 {
-  //PINA = _BV(PINA6);
-  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (100));
+  PINB = _BV(PINB5);
+  muos_clpq_repeat (event, MUOS_CLOCK_MILLISECONDS (250));
 }
 
 
-
-void
-toggle_led_timed2 (const struct muos_spriq_entry* event)
-{
-  //PINA = _BV(PINA0) | _BV(PINA1) | _BV(PINA2) | _BV(PINA3) | _BV(PINA4) | _BV(PINA5) | _BV(PINA6);
-  muos_clpq_repeat (event, MUOS_CLOCK_MICROSECONDS (2000));
-}
-
-
-void
-toggle_led (void)
-{
-  //PINA = _BV(PINA5);
-  //PINA = _BV(PINA5);
-  muos_bgq_pushback (toggle_led);
-  //PINA = _BV(PINA5);
-}
 
 void
 error (void)
@@ -64,62 +45,8 @@ lineecho (const char* line)
   muos_output_cstr ("\r\n<");
   muos_output_cstr (line);
   muos_output_cstr (">\r\n");
-#if 0 //hexdump
-  muos_output_cstr ("<");
-  while (*line)
-    {
-      muos_output_base (16);
-      muos_output_uint8 (*line);
-      muos_output_char (' ');
-      ++line;
-    }
-  muos_output_cstr (">\r\n");
-#endif
 }
 
-void
-cppm_output (void)
-{
-#if 0
-  //PINA = _BV(PINA2);
-
-  //if (muos_cppm_channel_cooked[0] > 0)
-  //  PORTA |= _BV(PINA0);
-  //else
-  //  PORTA &= ~_BV(PINA0);
-
-  //if (muos_cppm_channel_cooked[1] >= 0)
-  //  PORTA |= _BV(PINA1);
-  //else
-  //  PORTA &= ~_BV(PINA1);
-
-#if 0
-  if (muos_cppm_channel[2] > MUOS_CLOCK_MICROSECONDS(1500))
-    PORTA |= _BV(PINA2);
-  else
-    PORTA &= ~_BV(PINA2);
-
-  if (muos_cppm_channel[3] > MUOS_CLOCK_MICROSECONDS(1500))
-    PORTA |= _BV(PINA3);
-  else
-    PORTA &= ~_BV(PINA3);
-
-  if (muos_cppm_channel[4] > MUOS_CLOCK_MICROSECONDS(1500))
-    PORTA |= _BV(PINA4);
-  else
-    PORTA &= ~_BV(PINA4);
-  if (muos_cppm_channel[5] > MUOS_CLOCK_MICROSECONDS(1500))
-    PORTA |= _BV(PINA5);
-  else
-    PORTA &= ~_BV(PINA5);
-
-  if (muos_cppm_channel[6] > MUOS_CLOCK_MICROSECONDS(1500))
-    PORTA |= _BV(PINA6);
-  else
-    PORTA &= ~_BV(PINA6);
-#endif
-#endif
-}
 
 void
 hello_world (void)
@@ -132,14 +59,8 @@ hello_world (void)
 void
 init (void)
 {
-  //DDRA = _BV(PINA0) | _BV(PINA1) | _BV(PINA2) | _BV(PINA3) | _BV(PINA4) | _BV(PINA5) | _BV(PINA6);
-  //  PINA = _BV(PINA0) | _BV(PINA2) | _BV(PINA4) | _BV(PINA6);
+  DDRB = _BV(PINB5);
 
-  // PINA = _BV(PINA1);
-  //  GIMSK |= _BV(PCIE);
-  //PCMSK |= _BV(PCINT2);
-
-  //muos_clpq_at (0, MUOS_CLOCK_MILLISECONDS (100), toggle_led_timed);
-  //muos_clpq_at (0, MUOS_CLOCK_MILLISECONDS (1), toggle_led_timed2);
+  muos_clpq_at (0, MUOS_CLOCK_MILLISECONDS (250), toggle_led_timed);
   muos_bgq_pushback (hello_world);
 }
