866936e4a1794e873f22de32e9e05f983d216d29
Christian Thäter <ct@wolke.pipapo.org>
2018-11-28 22:06:58 +0100

    Basic test_stepper version

--- src/test_stepper.c
+++ src/test_stepper.c
@@ -15,9 +15,10 @@
 #include <muos/serial.h>
 #include <muos/io.h>
 #include <muos/error.h>
+#include <muos/stepper.h>
 
 // define events, no main here
-#define BLINK MUOS_CLOCK_MILLISECONDS (50)
+#define BLINK MUOS_CLOCK_MILLISECONDS (500)
 
 void
 toggle_led_timed (const struct muos_spriq_entry* event)
@@ -26,14 +27,6 @@ toggle_led_timed (const struct muos_spriq_entry* event)
   MUOS_DEBUG_C1_TOGGLE;
 
   muos_clpq_repeat (event, BLINK);
-}
-
-
-
-void
-error (void)
-{
-  // don't handle errors, should be done in real code here
 }
 
 
@@ -46,20 +39,69 @@ lineecho (const char* line)
   muos_output_cstr (">\r\n");
 }
 
-
 void
-hello_world (void)
+error (void)
+{
+  // don't handle errors, should be done in real code here
+}
+
+#if 0
+void config_loaded (void)
 {
   muos_interrupt_enable ();
-  muos_output_cstr ("Hello World");
-  muos_output_nl ();
+
+  muos_configstore_status status = muos_configstore_get_status ();
+
+  if (status != CONFIGSTORE_VALID)
+    {
+      if (status == CONFIGSTORE_INVALID)
+        {
+          //set defaults, (later save)
+        }
+      else
+        {
+          // panic
+          for (;;)
+            MUOS_DEBUG_ERROR_TOGGLE;
+        }
+    }
+
+  // now play with steppers
 }
+#endif
+
+
+
+void
+stepper_test (void)
+{
+  muos_interrupt_enable ();
+
+  muos_output_cstr_P ("Starting stepper ");
+  muos_output_nl ();
+
+  //muos_error err =
+  muos_stepper_cal_mov (0,
+                        2,
+                        0xfff0,
+                        1000);
+
+  muos_output_uint16 (TCNT1);
+  muos_output_nl ();
+
+}
+
+
 
 
 void
 init (void)
 {
-  muos_clpq_at (0, BLINK, toggle_led_timed);
-  muos_bgq_pushback (hello_world);
+  muos_interrupt_enable ();
+  muos_output_cstr_P ("Init ");muos_output_nl ();
+
+  //muos_clpq_at (0, MUOS_CLOCK_MILLISECONDS (1000), toggle_led_timed);
+
+  muos_hpq_pushback(stepper_test);
 }
 
