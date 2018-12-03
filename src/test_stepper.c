8ae4a74dcbd2e47b198a4ab8e091d7e25fa0c495
Christian Thäter <ct@wolke.pipapo.org>
2018-12-03 21:13:26 +0100

    big test_stepper update with all recent changes

--- src/test_stepper.c
+++ src/test_stepper.c
@@ -16,6 +16,7 @@
 #include <muos/io.h>
 #include <muos/error.h>
 #include <muos/stepper.h>
+#include <muos/configstore.h>
 
 // define events, no main here
 #define BLINK MUOS_CLOCK_MILLISECONDS (500)
@@ -24,7 +25,7 @@ void
 toggle_led_timed (const struct muos_spriq_entry* event)
 {
   muos_interrupt_enable ();
-  MUOS_DEBUG_C1_TOGGLE;
+  //MUOS_DEBUG_ERROR_TOGGLE;
 
   muos_clpq_repeat (event, BLINK);
 }
@@ -45,30 +46,33 @@ error (void)
   // don't handle errors, should be done in real code here
 }
 
-#if 0
 void config_loaded (void)
 {
   muos_interrupt_enable ();
 
-  muos_configstore_status status = muos_configstore_get_status ();
-
-  if (status != CONFIGSTORE_VALID)
+  if (muos_error_check(muos_error_configstore_invalid))
     {
-      if (status == CONFIGSTORE_INVALID)
-        {
-          //set defaults, (later save)
-        }
-      else
-        {
-          // panic
-          for (;;)
-            MUOS_DEBUG_ERROR_TOGGLE;
-        }
+      //set defaults, (later save)
+
     }
 
   // now play with steppers
+
+  muos_output_cstr_P ("Starting stepper ");
+  muos_output_nl ();
+
+  muos_wait (0, 0, MUOS_CLOCK_MILLISECONDS (500));
+
+  muos_error_set(muos_stepper_all_on ());
+  muos_error_set(muos_stepper_set_zero (0, 100));
+
+  muos_wait (0, 0, MUOS_CLOCK_MILLISECONDS(250));
+
+  //  muos_stepper_move_abs (0, 0, 255);
+
+
+  muos_stepper_all_off ();
 }
-#endif
 
 
 
@@ -80,18 +84,22 @@ stepper_test (void)
   muos_output_cstr_P ("Starting stepper ");
   muos_output_nl ();
 
-  //muos_error err =
-  muos_stepper_cal_mov (0,
-                        2,
-                        0xfff0,
-                        1000);
+  muos_wait (0, 0, MUOS_CLOCK_MILLISECONDS (500));
 
-  muos_output_uint16 (TCNT1);
-  muos_output_nl ();
+  //muos_error err =
+  muos_error_set(muos_stepper_cal_mov (0,
+                                       2,
+                                       5000, //0xfff0,
+                                       100));
+
+  muos_wait (0, 0, MUOS_CLOCK_MILLISECONDS(250));
+
+  muos_error_set(muos_stepper_cal_mov (0,
+                                       2,
+                                       5000, //0xfff0,
+                                       -100));
 
 }
-
-
 
 
 void
@@ -100,8 +108,11 @@ init (void)
   muos_interrupt_enable ();
   muos_output_cstr_P ("Init ");muos_output_nl ();
 
-  //muos_clpq_at (0, MUOS_CLOCK_MILLISECONDS (1000), toggle_led_timed);
+  //  muos_clpq_at (0, MUOS_CLOCK_MILLISECONDS (1000), toggle_led_timed);
 
-  muos_hpq_pushback(stepper_test);
+  //  muos_hpq_pushback(stepper_test);
+
+  muos_configstore_load (config_loaded);
+
 }
 
