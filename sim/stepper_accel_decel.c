27ba38db1c8c12bb0a70f1f91c7921f466707a6f
Christian Thäter <ct@wolke.pipapo.org>
2018-12-09 22:10:47 +0100

    stash some tests

--- sim/stepper_accel_decel.c
+++ sim/stepper_accel_decel.c
@@ -13,13 +13,16 @@ uint32_t CLOCK;    // clock ticks
 uint16_t OCRA;     // pulse length
 
 int32_t position;
-int32_t position_end = 1500;
+int32_t position_end = 1024;
 
-uint16_t min_speed = 60000;
-uint16_t max_speed = 1000;
+const uint16_t min_speed = 65535;
+const uint16_t max_speed = 1024;
 
-uint16_t accel = 200;
-uint16_t decel = 255;
+//TODO: max_slope var, code for long movements
+uint16_t max_slope = 65000;
+
+const uint8_t accel = 15;
+const uint8_t decel = 15;
 
 int32_t start;
 int32_t end;
@@ -39,30 +42,49 @@ init (void)
   PRINT(start);
   PRINT(end);
 
-  printf("%u %u %u\n", 0, position, (655360)/(min_speed+1));
+  printf("%u %u %u %u\n", 0, position, (65536*16)/(min_speed+1), min_speed);
   uint32_t speed=OCRA;
   PRINT(speed);
 }
 
+
+
+
+uint16_t
+usqrt16 (uint16_t x)
+{
+  uint16_t i;
+  for (i=1; i<256 && i*i<x; ++i);
+  return i;
+}
 
 bool
 overflow_isr(void)
 {
   CLOCK += OCRA;
   ++position;
-
-  uint32_t speed = ((65536*256)/((position-start)*accel+256)
-                    + (65536*256)/((end-position)*decel+256))
-    + max_speed;
-
-
-  PRINT(speed);
-  printf("%u %u %u\n", CLOCK, position, (655360)/(speed+1));
-
-  OCRA = speed<min_speed?speed:min_speed;
-
   if (position == position_end)
     return false;
+
+  //TODO: init these as constants
+  const uint32_t M  = (65535-max_speed)*256;
+  //const uint16_t OFFSET = M/(min_speed-max_speed+1)+236;  // end = 4
+  //const uint16_t OFFSET = M/(min_speed-max_speed+1)+210;  // end = 8
+  //const uint16_t OFFSET = M/(min_speed-max_speed+1)+168;  // end = 16
+  //const uint16_t OFFSET = M/(min_speed-max_speed+1)+18;  // end = 255
+  // const uint16_t OFFSET = M/(min_speed-max_speed+1)+5;  // end = 1024
+  //const uint16_t OFFSET = M/(min_speed-max_speed+1)+1;  // end = 8192
+  const uint16_t OFFSET = M/(min_speed-max_speed);  // end = 8192
+
+  uint32_t a = (position-start+1)*accel+OFFSET;
+  uint32_t d = (end-position-1)*decel+OFFSET;
+  uint32_t speed = M/a + M/d + max_speed;
+
+  PRINT(speed);
+  printf("%u %u %u %u\n", CLOCK, position, (uint32_t)((65536*16)/(speed+1)), speed);
+
+  OCRA = speed;
+
 
   return true;
 }
