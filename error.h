acb34882319199803d19a7cb963ba2043ac0eca7
Christian Thäter <ct@wolke.pipapo.org>
2019-01-03 12:30:25 +0100

    FIX: MUOS_OK doh

--- error.h
+++ error.h
@@ -34,7 +34,7 @@
 //: ----
 //: Wraps fn which must be a function call returning a 'muos_error' an a check for success
 //: or returning the error.
-#define MUOS_OK(fn) do { muos_error err = fn; if (!err) return err; } while(0)
+#define MUOS_OK(fn) do { muos_error err = fn; if (err) return err; } while(0)
 
 //TODO: conditional error generation depending on what drivers are enabled
 //defined_errors:
