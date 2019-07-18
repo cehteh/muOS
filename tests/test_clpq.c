28df4cf83bd0a0be926470bfbe2c8cc187d59b7b
Christian Thäter <ct@debian.pipapo.org>
2019-07-18 19:44:07 +0200

    WIP: clpq_test update to new api

--- tests/test_clpq.c
+++ tests/test_clpq.c
@@ -1,4 +1,4 @@
-// gcc -g -Wall -Wextra -Werror -I../.. test_spriq.c && ./a.out
+// gcc -g -Wall -Wextra -Werror -I../.. test_clpq.c && ./a.out
 
 
 
@@ -18,10 +18,8 @@
 
 
 #define MUOS_CLPQ_LENGTH 16
-//#define MUOS_CLOCK_TYPE uint32_t
-//#define PD "%d"
-#define MUOS_CLOCK_TYPE uint64_t
-#define PD "%ld"
+#define MUOS_CLOCK_SIZE 8
+#define PD "%d"
 
 
 #define MUOS_CLPQ_BARRIERS 1  // initial implementation simple barriers
@@ -47,14 +45,187 @@ void
 muos_interrupt_disable (void)
 {}
 
+#include <muos/error.h>
 
-typedef MUOS_CLOCK_TYPE muos_clock;
-typedef uint32_t muos_clock32;
-typedef uint16_t muos_clock16;
+
+
+#include "muos/lib/clock.h"
+
 
 muos_clock clock_coarse;
 
 
+
+void
+muos_clock_now_isr (muos_clock* now)
+{
+  muos_clock_copy (now, &clock_coarse);
+  muos_clock_add8 (now, fake_hw_clock1);
+}
+
+
+muos_error
+muos_error_set_isr (muos_error err)
+{
+  return err;
+}
+
+
+void
+clock_tick (void)
+{
+  if (!++fake_hw_clock1)
+    muos_clock_add16 (&clock_coarse, 256);
+
+  muos_clock now;
+  muos_clock_now_isr (&now);
+
+  printf ("now: "PD"/"PD"\n",
+          muos_clock_clock32 (&now),
+          muos_clock_clock16 (&now));
+}
+
+
+void
+clpq_dumpp (const char* name);
+
+
+
+/*
+
+  Tests here
+
+*/
+
+
+
+#include "muos/clpq.c"
+
+void
+hello (void)
+{
+  printf("\thello \n");
+}
+
+void
+world (void)
+{
+  printf("\tworld \n");
+}
+
+
+#define BLINK 2510
+void
+blink (void)
+{
+  printf("\tblink \n");
+
+  muos_clpq_repeat (BLINK);
+  clpq_dumpp ("blink rep");
+}
+
+
+#define PING 19990
+void
+ping (void)
+{
+  printf("\tping \n");
+
+  muos_clpq_repeat (PING);
+  clpq_dumpp ("ping rep");
+}
+
+
+#define LIVE 50
+void
+live (void)
+{
+  printf("\tlive \n");
+
+  muos_clpq_repeat (LIVE);
+  clpq_dumpp ("live rep");
+}
+
+
+const char*
+fname(muos_clpq_function fn)
+{
+  if (fn == NULL) return "NULL";
+  if (fn == hello) return "hello";
+  if (fn == world) return "world";
+  if (fn == blink) return "blink";
+  if (fn == ping) return "ping";
+  if (fn == live) return "live";
+  return "BARRIER";
+}
+
+void
+clpq_dumpp (const char* name)
+{
+  (void) name;
+  printf ("DUMP: %s: used %d:\n", name, muos_clpq.used);
+  //for (uint8_t i = 0; i < MUOS_CLPQ_LENGTH; ++i)
+  for (uint8_t i = 0; i < muos_clpq.used; ++i)
+    {
+      printf("  %d: %d %p = %s\n", i, muos_clpq.entries[i].when, muos_clpq.entries[i].what, fname(muos_clpq.entries[i].what));
+    }
+  printf("---\n\n");
+}
+
+
+#define OK(call) do { printf("\n" #call ": "); if(call) printf("FAILED"); printf("\n\n"); } while (0)
+
+
+
+
+
+
+int
+main (int argc, char* argv[])
+{
+  (void) argc; (void) argv;
+  printf("# Test start...\n\n");
+
+  clpq_dumpp ("empty");
+
+
+  muos_clpq_after (10, blink);
+  muos_clpq_after (100, blink);
+  muos_clpq_after (50, ping);
+  muos_clpq_after (1150, live);
+  clpq_dumpp ("initialized");
+
+  printf ("Clock ticking...\n");
+
+
+  muos_clock now;
+  muos_clock_now_isr (&now);
+
+  while (muos_clock_clock32 (&now) < 1655350)
+    {
+      clock_tick ();
+
+      while (muos_clpq_schedule_isr ())
+        {
+          clpq_dumpp ("schedule");
+        }
+
+
+
+      muos_clock_now_isr (&now);
+    }
+
+  printf("\n#...Test done\n");
+  return 0;
+}
+
+
+
+
+
+
+
+#if 0 //ATTIC
 muos_clock
 muos_clock_now (void)
 {
@@ -99,157 +270,4 @@ muos_clock16_elapsed (muos_clock16 now, muos_clock16 start)
 }
 
 
-
-/*
-
-  Tests here
-
-*/
-
-
-
-#include "muos/clpq.h"
-#include "muos/clpq.c"
-
-muos_error
-muos_error_set_isr (muos_error err)
-{
-  return err;
-}
-
-extern muos_clpq_type muos_clpq;
-
-void
-clock_tick (void)
-{
-  if (!++fake_hw_clock1)
-    clock_coarse += 256;
-
-  //printf ("now: "PD"/"PD"\n", muos_clpq.now, muos_clpq.now%65536);
-}
-
-
-void
-clpq_dumpp (const char* name);
-
-
-void
-hello (muos_clock16 delayed)
-{
-  printf("\thello %d\n", delayed);
-}
-
-void
-world (muos_clock16 delayed)
-{
-  printf("\tworld %d\n", delayed);
-}
-
-
-#define BLINK 2510
-void
-blink (muos_clock16 delay)
-{
-  printf("\tblink %d\n", delay);
-
-  muos_clpq_at (muos_clpq_now () + BLINK - delay, blink);
-  clpq_dumpp ("blink rep");
-}
-
-
-#define PING 19990
-void
-ping (muos_clock16 delay)
-{
-  printf("\tping %d\n", delay);
-
-  muos_clpq_at (muos_clpq_now () + PING - delay, ping);
-  clpq_dumpp ("ping rep");
-}
-
-
-#define LIVE 50
-void
-live (muos_clock16 delay)
-{
-  printf("\tlive %d\n", delay);
-
-  muos_clpq_at (muos_clpq_now () + LIVE - delay, live);
-  clpq_dumpp ("live rep");
-}
-
-
-const char*
-fname(muos_clpq_function fn)
-{
-  if (fn == NULL) return "NULL";
-  if (fn == hello) return "hello";
-  if (fn == world) return "world";
-  if (fn == blink) return "blink";
-  if (fn == ping) return "ping";
-  if (fn == live) return "live";
-  return "BARRIER";
-}
-
-void
-clpq_dumpp (const char* name)
-{
-  printf("DUMP: %s: now "PD"/"PD": used %d:\n", name, muos_clpq.now, muos_clpq.now%65536, muos_clpq.used);
-  //for (uint8_t i = 0; i < MUOS_CLPQ_LENGTH; ++i)
-  for (uint8_t i = 0; i < muos_clpq.used; ++i)
-    {
-      printf("  %d: %d %p = %s\n", i, muos_clpq.entries[i].when, muos_clpq.entries[i].what, fname(muos_clpq.entries[i].what));
-    }
-  printf("---\n\n");
-}
-
-
-#define OK(call) do { printf("\n" #call ": "); if(call) printf("FAILED"); printf("\n\n"); } while (0)
-
-
-
-
-
-
-int
-main (int argc, char* argv[])
-{
-  (void) argc; (void) argv;
-  printf("# Test start...\n\n");
-
-  clpq_dumpp ("empty");
-
-
-  //  muos_clpq_at (0, blink);
-  muos_clpq_at (10, blink);
-  muos_clpq_at (50, ping);
-  muos_clpq_at (1150, live);
-  clpq_dumpp ("blink");
-
-  printf ("Clock ticking...\n");
-  while (muos_clpq_now () < 16553500)
-    {
-      clock_tick ();
-      //printf("s %d\n", muos_clpq_schedule_isr ());
-      muos_clpq_schedule_isr ();
-#if 0
-      while (muos_clpq_schedule_isr ())
-        {
-          clpq_dumpp ("schedule");
-        }
 #endif
-    }
-
-  printf("\n#...Test done\n");
-  return 0;
-}
-
-
-
-
-
-
-
-
-
-
